dac82510866ef05f5f1aa6f4a7512d0f66bedcaf
eddieantola <antola.edoardo@gmail.com>
2024-05-29 17:25:21 +0200

    inserimento di commenti

--- src/main/java/com/majorbit/springboot_tre/TaskService.java
+++ src/main/java/com/majorbit/springboot_tre/TaskService.java
@@ -9,9 +9,11 @@ import com.majorbit.springboot_tre.model.dao.DAOTask;
 @Service
 public class TaskService {
 
+  //injection della repository delle task
   @Autowired
   private DAOTask dao;
 
+  //metodo per passare alla repository il task da aggiungere
   public String createTask(Task t) {
     int r = dao.createTask(t);
     if (r>0) {
@@ -20,9 +22,13 @@ public class TaskService {
       return "Ops, qualcosa è andato storto!";
     }
   }
+
+  //metodo per passare alla repository l'id per il recupero del task richiesto 
   public Task readTask(int id) {
     return dao.readTask(id);
   }
+
+  //metodo per passare alla repository l'id del task da modificare e un nuovo task da sostuire ad esso
   public String updateTask(int id, Task t) {
     int r = dao.updateTask(id, t);
     if (r>0) {
@@ -31,6 +37,8 @@ public class TaskService {
       return "Ops, qualcosa è andato storto!";
     }
   }
+
+  //metodo per passare alla repository l'id del task da eliminare
   public String deleteTask(int id) {
     int r = dao.deleteTask(id);
     if (r>0) {
